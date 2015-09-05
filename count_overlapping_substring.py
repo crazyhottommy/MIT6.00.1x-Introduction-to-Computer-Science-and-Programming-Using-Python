ab3678e10fa785fe059d5947684484934a2e7dac
Ming Tang <tangming2005@gmail.com>
2015-09-04 20:48:10 -0500

    Create count_overlapping_substring.py

---
+++ count_overlapping_substring.py
@@ -0,0 +1,11 @@
+## function('1011101111','11')
+## returns 5
+
+def occurrences(string, sub):
+    count = start = 0
+    while True:
+        start = string.find(sub, start) + 1
+        if start > 0:
+            count+=1
+        else:
+            return count
