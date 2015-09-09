ae60343a0000dad58dbc9cb7a733f84d8038e902
Ming Tang <tangming2005@gmail.com>
2015-09-08 22:31:19 -0500

    Create week3_recursive_powe_newr.py

---
+++ week3_recursive_powe_newr.py
@@ -0,0 +1,14 @@
+def recurPowerNew(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+
+    returns: int or float; base^exp
+    '''
+    # Your code here
+    if exp == 0:
+        return 1
+    elif exp > 0 and exp %2 == 1:
+        return base * recurPowerNew(base, exp -1)
+    elif exp > 0 and exp %2 ==0:
+        return recurPowerNew(base*base, exp/2)
